93ed9614feb92e5a0bc69693a25328b726c6a0ef
TammyH <tmhiggins1986@gmail.com>
2025-01-28 13:58:06 -0600

    new files

---
+++ src/utils/output.js
@@ -0,0 +1,9 @@
+export function output(
+     message = "",
+     outputTag = "outputTag", 
+     shouldAppend = true
+    ) {
+    if (shouldAppend) window[outputTag].innerHTML += message;
+    else window[outputTag].innerHTML = message;
+ }
+
