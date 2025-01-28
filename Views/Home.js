1d8187488c3845de5955c67b1f5a266a0be6660c
TammyH <tmhiggins1986@gmail.com>
2025-01-28 14:17:00 -0600

    New files

---
+++ Views/Home.js
@@ -0,0 +1,16 @@
+import  React from "react"
+import { Link } from "react-router"
+import "./home.scss"
+
+<>
+<header>
+        <nav>
+          
+          <Link to="/">Home | </Link>
+          <Link>About | </Link>
+          <Link>Contact | </Link>
+          <Link to="/greeting">Greeting | </Link>
+          <Link to="/videos">videos</Link>
+        </nav>
+      </header>
+</>
